e8596a7ee7ed3bf714fb84a3466bf5361c5cc6a2
imxood <imxood@gmail.com>
2019-10-20 09:18:08 +0800

    .

--- py/ssh_tools.py
+++ py/ssh_tools.py
@@ -6,6 +6,8 @@ import time
 import logging
 import threading
 from contextlib import contextmanager
+from subprocess import Popen, PIPE
+import signal
 
 
 class ExceptionHandlingThread(threading.Thread):
@@ -17,22 +19,14 @@ class ExceptionHandlingThread(threading.Thread):
 
     def run(self):
         try:
-            if hasattr(self, "_run") and callable(self._run):
-                self._run()
-            else:
-                super(ExceptionHandlingThread, self).run()
+            super(ExceptionHandlingThread, self).run()
         except BaseException:
-            # Store for actual reraising later
             self.exc_info = sys.exc_info()
-            # And log now, in case we never get to later (e.g. if executing
-            # program is hung waiting for us to do something)
             msg = "Encountered exception {!r} in thread for {!r}"
-            # Name is either target function's dunder-name, or just "_run" if
-            # we were run subclass-wise.
             name = "_run"
             if "target" in self.kwargs:
                 name = self.kwargs["target"].__name__
-            print(msg.format(self.exc_info[1], name))  # noqa
+            print(msg.format(self.exc_info[1], name))
 
 
 class Runner:
@@ -43,10 +37,21 @@ class Runner:
         pass
 
     def read_proc_stdout(self, num_bytes):
-        pass
+        raise NotImplementedError
 
     def read_proc_stderr(self, num_bytes):
-        pass
+        raise NotImplementedError
+
+    def stop(self):
+        raise NotImplementedError
+
+    @property
+    def process_is_finished(self):
+        raise NotImplementedError
+
+    @property
+    def returncode(self):
+        raise NotImplementedError
 
     def read_proc_output(self, reader):
         while True:
@@ -75,15 +80,84 @@ class Runner:
             buffer_, hide, output, reader=self.read_proc_stderr
         )
 
+    def _run(self, hide, out_stream=sys.stdout, err_stream=sys.stderr):
+
+        thread_args = dict()
+
+        output = []
+        error = []
+
+        thread_args[self.handle_stdout] = {
+            'buffer_': output,
+            'hide': hide,
+            'output': out_stream
+        }
+
+        thread_args[self.handle_stderr] = {
+            'buffer_': error,
+            'hide': hide,
+            'output': err_stream
+        }
+
+        for target, kwargs in thread_args.items():
+            t = ExceptionHandlingThread(target=target, kwargs=kwargs)
+            t.start()
+
+        while True:
+            if self.process_is_finished:
+                break
+            time.sleep(0.01)
+
+        self.stop()
+
+        return (self.returncode, ''.join(output), ''.join(error))
+
+
+class Local(Runner):
+
+    def __init__(self, *args, **kwargs):
+        super(Local, self).__init__(*args, **kwargs)
+
+    def start(self, command, hide, env=None):
+        self.process = Popen(
+            command,
+            shell=True,
+            env=env,
+            stdout=PIPE,
+            stderr=PIPE,
+            stdin=PIPE,
+        )
+        super()._run(hide)
+
+    def read_proc_stdout(self, num_bytes):
+        return os.read(self.process.stdout.fileno(), num_bytes)
+
+    def read_proc_stderr(self, num_bytes):
+        return os.read(self.process.stderr.fileno(), num_bytes)
+
+    def stop(self):
+        self.process.kill()
+
+    @property
+    def process_is_finished(self):
+        if self.process.poll():
+            return False
+        return True
+
+    @property
+    def returncode(self):
+        return self.process.returncode
+
 
 class Remote(Runner):
 
     def __init__(self, *args, **kwargs):
         super(Remote, self).__init__(*args, **kwargs)
 
-    def start(self, command, channel):
+    def start(self, command, channel, hide):
         self.channel = channel
         self.channel.exec_command(command)
+        super()._run(hide)
 
     def read_proc_stdout(self, num_bytes):
         return self.channel.recv(num_bytes)
@@ -91,8 +165,8 @@ class Remote(Runner):
     def read_proc_stderr(self, num_bytes):
         return self.channel.recv_stderr(num_bytes)
 
-    def _write_proc_stdin(self, data):
-        return self.channel.sendall(data)
+    def stop(self):
+        self.channel.close()
 
     @property
     def process_is_finished(self):
@@ -101,9 +175,6 @@ class Remote(Runner):
     @property
     def returncode(self):
         return self.channel.recv_exit_status()
-
-    def stop(self):
-        self.channel.close()
 
 
 class RemoteShell:
@@ -128,15 +199,10 @@ class RemoteShell:
         self.transport = None
         self._sftp = None
 
-        self.client.connect('127.0.0.1', port=2222, username='root')
-
-        self.log.debug('ssh shell __enter__')
-
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
-        self.log.debug('ssh shell __exit__')
 
     def _open(self):
         if self.is_connected:
@@ -173,26 +239,79 @@ class RemoteShell:
         paths = [path.replace(" ", r"\ ") for path in self.command_cwds[i:]]
         return os.path.join(*paths)
 
-    def get(self, remote_file='', local_file=''):
-        try:
-            self.sftp.get(remote_file, local_file)
-            self.log.debug("transfer successed!")
-            return (0, 'transfer successed!', '')
-        except IOError as e:
-            self.log.debug('transfer failed! {}'.format(e))
-            return (-1, 'transfer failed!', '')
-
     def put(self, local_file='', remote_file='', recursive=False):
+        """upload files or directory to remote server
+        """
         try:
+            if os.path.isdir(local_file):
+                if recursive == False:
+                    return False
+
+                name = os.path.basename(local_file)
+
+                local_root = local_file
+
+                if name != os.path.basename(remote_file):
+                    remote_root = '{}/{}'.format(remote_file, name)
+                else:
+                    remote_root = remote_file
+
+                if not self.exist(remote_root):
+                    self.mkdir(remote_root)
+
+                for root, dirs, files in os.walk(local_root):
+                    for dir in dirs:
+                        remote_dir = os.path.relpath('{}/{}'.format(root, dir), local_root)
+                        remote_dir = '{}/{}'.format(remote_root, remote_dir)
+                        self.mkdir(remote_dir)
+                    for file in files:
+                        local_file = '{}/{}'.format(root, file)
+                        remote_file = os.path.relpath(local_file, local_root)
+                        remote_file = '{}/{}'.format(remote_root, remote_file)
+                        self.sftp.put(local_file, remote_file)
+                return True
+
             self.sftp.put(local_file, remote_file)
-            self.log.debug("transfer successed!")
-            return (0, 'transfer successed!', '')
+            return True
+
         except IOError as e:
             self.log.debug('transfer failed! {}'.format(e))
-            return (-1, 'transfer failed!', '')
+            return False
 
-    def run(self, cmd, hide=False):
+    def exist(self, path):
+        """judge whether the path existes
+        """
+        try:
+            self.sftp.stat(path)
+        except IOError as e:
+            if 'No such file' in str(e):
+                return False
+            raise
+        else:
+            return True
 
+    def mkdir(self, path, ignore_existed=True):
+        """make a directory, if ignore_existed == True, it will happen error if the dir existes
+        """
+        try:
+            self.sftp.mkdir(path)
+        except IOError:
+            if ignore_existed:
+                pass
+            else:
+                raise
+
+    def local(self, cmd, hide=False):
+        '''run local command
+        '''
+        command = cmd
+
+        local = Local()
+        local.start(command, hide)
+
+    def remote(self, cmd, hide=False):
+        '''run remote command
+        '''
         self._open()
 
         command = self._prefix_commands(cmd)
@@ -200,35 +319,7 @@ class RemoteShell:
         channel = self.client.get_transport().open_session()
 
         remote = Remote()
-        remote.start(command, channel)
-
-        thread_args = dict()
-
-        output = []
-        error = []
-
-        thread_args[remote.handle_stdout] = {
-            'buffer_': output,
-            'hide': hide,
-            'output': sys.stdout
-        }
-
-        thread_args[remote.handle_stderr] = {
-            'buffer_': error,
-            'hide': hide,
-            'output': sys.stderr
-        }
-
-        for target, kwargs in thread_args.items():
-            t = ExceptionHandlingThread(target=target, kwargs=kwargs)
-            t.start()
-
-        while True:
-            if remote.process_is_finished:
-                break
-            time.sleep(0.01)
-
-        return (remote.returncode, ''.join(output), ''.join(error))
+        remote.start(command, channel, hide)
 
     @property
     def is_connected(self):
@@ -244,15 +335,18 @@ class RemoteShell:
 
 def test():
 
-    with RemoteShell() as s:
-        s.run('ls -al')
+    with RemoteShell(port=2222) as s:
+        s.local('ls -al')
+        s.remote('mkdir -p test1/test2/test3')
+        s.remote('ls -al')
         with s.cd('test1'):
-            s.run('ls -al')
+            s.put('web', 'test1', recursive=True)
+            s.remote('ls -al')
             with s.cd('test2'):
-                s.run('ls -al')
+                s.remote('ls -al')
                 with s.cd('test3'):
-                    s.run('ls -al', hide=True)
-        s.run('dmesg')
+                    s.remote('ls -al', hide=True)
+        s.remote('dmesg')
         # s.sftp.mkdir('test1_1')
 
 
