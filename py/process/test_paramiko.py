1bb22a16145c91a39cc2c2ef21bf3f7448d73974
hello <Imxood@gmail.com>
2020-09-18 17:34:47 +0800

    Update test_paramiko.py

--- py/process/test_paramiko.py
+++ py/process/test_paramiko.py
@@ -7,6 +7,7 @@ import logging
 import tarfile
 import io
 from pathlib import Path
+from contextlib import contextmanager
 
 from paramiko import SSHClient, AutoAddPolicy
 
@@ -76,12 +77,21 @@ class Remote():
 
         # get the index for the subset of paths starting with the last / or ~
         for i, path in reversed(list(enumerate(self.command_cwds))):
-            if not isinstance(path, str):
+            if not isinstance(path, str) and not isinstance(path, Path):
                 return ""
+            path = str(path)
             if path.startswith("~") or path.startswith("/"):
                 break
-        paths = [path.replace(" ", r"\ ") for path in self.command_cwds[i:]]
+        paths = [str(path).replace(" ", r"\ ") for path in self.command_cwds[i:]]
         return os.path.join(*paths)
+
+    @contextmanager
+    def cd(self, path):
+        self.command_cwds.append(path)
+        try:
+            yield
+        finally:
+            self.command_cwds.pop()
 
     def _get_lines(self, stringio, text: str, terminator='\n'):
         pos = stringio.tell()
@@ -105,22 +115,26 @@ class Remote():
             # if the data is empty, the transmission ends
             data = deal_func(num_bytes)
             if not data:
+                if bufferQ:
+                    bufferQ.task_done()
                 break
             data = data.decode('utf-8', "replace")
-            lines = self._get_lines(buffer, data)
+            lines = self._get_lines(buffer, data, terminator)
             for line in lines:
                 if line:
                     if bufferQ:
-                        await bufferQ.put(line + terminator)
+                        await bufferQ.put(line)
                     if not hide:
                         self.log.info(line)
             # if len(lines) > 1 and lines[-1] == '':
             #     self.lastline = lines[-2]
         return data
 
-    async def run(self, cmd, hide=False, timeout=None, bufferQ: asyncio.Queue = None, num_bytes=1024):
+    async def run(self, cmd, hide=False, timeout=30, bufferQ: asyncio.Queue = None, num_bytes=1024):
         self.open()
+        self.log.info('run cmd: %s', cmd)
         command = self._prefix_commands(cmd)
+        self.log.debug('real cmd: %s', command)
         # open channel
         channel = self.transport.open_session()
         # set exec_command's timeout
@@ -128,11 +142,9 @@ class Remote():
         # exec command
         channel.exec_command(command)
         # wait the task's output
-        output_task = asyncio.ensure_future(
-            self.recv(channel.recv, num_bytes, hide, bufferQ))
-        output_err_task = asyncio.ensure_future(
-            self.recv(channel.recv_stderr, num_bytes, hide, bufferQ))
-        output, output_err = await asyncio.gather(output_task, output_err_task)
+        output_future = asyncio.ensure_future(self.recv(channel.recv, num_bytes, hide, bufferQ))
+        output_err_future = asyncio.ensure_future(self.recv(channel.recv_stderr, num_bytes, hide, bufferQ))
+        output, output_err = await asyncio.gather(output_future, output_err_future)
         # get tha command return code
         retcode = channel.recv_exit_status()
         # close channel
@@ -142,21 +154,21 @@ class Remote():
     def isdir(self, path):
         """ judge if the remote path existes """
         try:
-            return stat.S_ISDIR(self.sftp.stat(path).st_mode)
+            return stat.S_ISDIR(self.sftp.stat(str(path)).st_mode)
         except IOError:
             return False
 
     def mkdir(self, path):
         """ it need is '/' path if the path is multilevel"""
         """ the path look like c:/a/b/c below windows """
-        if self.isdir(path):
-            raise Exception('the directory specified already exists: ' + path)
+        # if self.isdir(path):
+        #     raise Exception('the directory specified already exists: ' + path)
         try:
-            dirs = path.split('/')
+            dirs = path.__str__().split('/')
             dir = dirs.pop(0)
             while True:
                 if not self.isdir(dir):
-                    self._sftp.mkdir(dir)
+                    self.sftp.mkdir(dir)
                 if len(dirs) == 0 or len(dirs) == 1 and dirs[0].strip() == '':
                     return
                 dir = dir + '/' + dirs.pop(0)
@@ -166,70 +178,72 @@ class Remote():
     def rm(self, path):
         """ delete remote file, not a dir """
         if self.exists(path) and not self.isdir(path):
-            self._sftp.remove(path)
+            self.sftp.remove(path)
 
-    def rm_dir(self, path):
+    def rmdir(self, path):
         """ delete remote dir, not a file """
 
         if self.exists(path):
 
-            files = self._sftp.listdir(path=path)
+            files = self.sftp.listdir(str(path))
 
             for f in files:
                 filepath = '{}/{}'.format(path, f)
                 if self.isdir(filepath):
-                    self.rm_dir(filepath)
+                    self.rmdir(filepath)
                 else:
-                    self._sftp.remove(filepath)
-            self._sftp.rmdir(path)
+                    self.sftp.remove(filepath)
+            self.sftp.rmdir(str(path))
 
-    def put(self, local_file, remote):
+    def put(self, local_file, remote_file, hide=False):
         """ upload local file to remote, is local file, not a local dir!! """
-
+        local_file = Path(local_file)
         ori_local = local_file
-        if not os.path.isabs(local_file):
-            local_file = os.path.join(os.getcwd(), local_file)
+        remote_file = Path(remote_file)
+        if not local_file.is_absolute():
+            local_file = Path(os.getcwd(), local_file)
 
-        if not os.path.exists(local_file):
+        if not local_file.exists():
             self.log.error('local file "{}" doesn\'t exist.'.format(ori_local))
             return False
 
         try:
-            if self.isdir(remote):
-                remote = os.path.join(remote, os.path.basename(local_file))
-            self._sftp.put(local_file, remote)
-            self.log.info(
-                '[local file] {} --> [remote file] {}'.format(local_file, remote))
+            if not self.isdir(remote_file.parent):
+                self.mkdir(remote_file.parent)
+            self.sftp.put(str(local_file), str(remote_file))
+            if not hide:
+                self.log.info('[local file] {} --> [remote file] {}'.format(local_file, remote_file))
             return True
         except IOError as e:
             self.log.error('transfer failed! err: {}'.format(e))
             return False
 
-    def put_dir(self, local_dir, dest_dir):
+    async def put_dir(self, local_dir, dest_dir, hide=False):
         """ upload local dir to remote, is local dir, not a local file!! """
 
-        target_dir = Path(dest_dir, Path(local_dir).name)
+        local_dir = Path(local_dir)
+        dest_dir = Path(dest_dir)
 
-        if self.exists(target_dir.__str__()):
-            self.rm_dir(target_dir.__str__())
+        target_dir = dest_dir / local_dir.name
 
-        local_tar_file = Path(tempfile.gettempdir(),
-                              os.path.basename(local_dir) + '.tar.gz')
+        if self.isdir(target_dir):
+            self.rmdir(target_dir)
+
+        local_tar_file = Path(tempfile.gettempdir(), local_dir.name + '.tar.gz')
         if local_tar_file.exists():
-            os.remove(local_tar_file)
+            local_tar_file.unlink()
 
-        self.__make_targz(local_dir, local_tar_file)
+        self._make_targz(local_dir, local_tar_file)
 
         remote_tar_file = Path(dest_dir, local_tar_file.name).__str__()
 
-        self.put(local_tar_file, remote_tar_file)
-        self.run('python3 -m tarfile -e {} {}'.format(remote_tar_file, dest_dir))
+        self.put(local_tar_file, remote_tar_file, hide=True)
+        await self.run('python3 -m tarfile -e {} {}'.format(remote_tar_file, dest_dir))
         self.rm(remote_tar_file)
 
-        os.remove(local_tar_file)
-
-        self.log.info(
-            '[local dir] {} --> [remote file] {}'.format(local_dir, dest_dir))
+        local_tar_file.unlink()
+        if not hide:
+            self.log.info('[local dir] {} --> [remote dir] {}'.format(local_dir, dest_dir))
 
         return True
 
@@ -237,7 +251,7 @@ class Remote():
         """judge whether the remote path existes
         """
         try:
-            self.sftp.stat(path)
+            self.sftp.stat(str(path))
         except IOError as e:
             if 'No such file' in str(e):
                 return False
@@ -245,7 +259,7 @@ class Remote():
         else:
             return True
 
-    def __make_targz(self, local_dir, output_filename):
+    def _make_targz(self, local_dir, output_filename):
         with tarfile.open(output_filename, 'w:gz') as tar:
             tar.add(local_dir, arcname=os.path.basename(local_dir))
 
@@ -274,10 +288,11 @@ tlog = logging.getLogger()
 async def serial_task():
 
     bufferQ = asyncio.Queue()
+    log = logging.getLogger()
 
-    with Remote('maxu', log=slog) as r:
+    with Remote('maxu', log=log) as r:
 
-        tlog.info('serial ...')
+        log.info('serial ...')
 
         command = 'plink -serial /dev/ttyUSB0 -sercfg 115200,8,n,1,N'
 
@@ -286,7 +301,7 @@ async def serial_task():
         while True:
             try:
                 line = await asyncio.wait_for(bufferQ.get(), timeout=10)
-                tlog.info(line)
+                log.info(line)
             except asyncio.QueueEmpty:
                 await task.cancel()
                 break
@@ -294,7 +309,7 @@ async def serial_task():
 
 
 async def main():
-    with Remote('maxu', log=slog) as r:
+    with Remote('maxu', log=logging.getLogger()) as r:
         ret = await r.run('ls -al')
         print(ret)
     await serial_task()
