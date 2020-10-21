1282987f5258dbacd5a070b6928ce0fbff083928
xumax <xux.ma@intel.com>
2020-10-21 14:14:41 +0800

    .

--- py/psutil/test_psutil.py
+++ py/psutil/test_psutil.py
@@ -1,39 +1,24 @@
-import json
-import psutil
-
-
-import os
-ref = {}
-for p in os.listdir("/proc/"):
-    if not p.isdigit():
-        continue
-    d = "/proc/%s/fd/" % p
-    try:
-        for fd in os.listdir(d):
-            f = os.readlink(d+fd)
-            if f not in ref:
-                ref[f] = []
-            ref[f].append(p)
-    except OSError:
-        pass
-for (k, v) in ref.items():
-    print(k, " ".join(v))
+import psutil as ps
+from pprint import pprint
 
 
 if __name__ == "__main__":
 
-    objs = []
+    try:
+        """ need privilege """
+        for proc in ps.process_iter():
+            print(proc.as_dict())
+        procs = {p.pid: p.info for p in ps.process_iter(
+            ['pid', 'name', 'username', 'cmdline'])}
+        pprint(procs)
+    except Exception as e:
+        print(e)
 
-    connections = open_files = list()
-
-    objs.append(connections)
-    objs.append(open_files)
-
-    for proc in psutil.process_iter():
+    for pid in ps.pids():
         try:
-            open_files.extend(proc.open_files())
+            proc = ps.Process(pid)
+            print(proc.name())
+            print(proc.username())
+            print(proc.cmdline())
         except Exception as e:
             print(e)
-
-    with open('test_psutil.json', 'w') as f:
-        json.dump(objs, f, indent=4)
