b671455496c1144f63ea3f49a329a215faf5729e
imxood <imxood@gmail.com>
2020-10-22 23:36:38 +0800

    .

--- py/log_server/log_server/install.sh
+++ py/log_server/log_server/install.sh
@@ -5,7 +5,4 @@ pip3 uninstall -y log_server
 rm -rf build/ dist/ log_server.egg-info/
 
 python3 setup.py sdist
-
-cd dist
-
-pip3 install --user log_server-0.1.tar.gz
+pip3 install --user dist/log_server-0.1.tar.gz
